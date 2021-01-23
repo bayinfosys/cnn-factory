d65ca1cf41081ca3dcd0dc16e0c42b0590b6767c
ed <anax@hotmail.co.uk>
2021-01-23 00:42:00 +0000

    BUGFIX: augmentation missing import and removed batch index

--- src/generators/augmentation.py
+++ src/generators/augmentation.py
@@ -1,5 +1,7 @@
 import logging
 
+import numpy as np
+
 import imgaug as ia
 import imgaug.augmenters as iaa
 
@@ -110,8 +112,8 @@ def create_image_augmentation_fn(num_augs = 4):
       aug_y = (aug_y > aug_y.mean()).astype(np.uint8) * 255
 
       # logger.debug("yielding: '%s'/'%s'" % (str(i_x.shape), str(i_y.shape)))
-      yield (aug_x[np.newaxis, ...].astype(np.float) / 255.0,
-             aug_y[np.newaxis, ...].astype(np.float) / 255.0)
+      yield (aug_x.astype(np.float) / 255.0,
+             aug_y.astype(np.float) / 255.0)
 
   if num_augs == 0:
     return None
